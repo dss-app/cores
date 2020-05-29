e99efe03f0fb426554a4883fcaffc04dbd39d0dd
tri3ans4h <tri3ans4h@gmail.com>
2020-05-30 05:39:37 +0700

    add certificates

--- cores/src/main/java/id/mydss/cores/ApiClient.java
+++ cores/src/main/java/id/mydss/cores/ApiClient.java
@@ -1,19 +1,27 @@
 package id.mydss.cores;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import javax.security.cert.CertificateException;
 
 import okhttp3.Interceptor;
@@ -93,8 +101,27 @@ public class ApiClient {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         //  OkHttpClient client = httpClient.addInterceptor(interceptor).build();
+        OkHttpClient client = null;
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+             client = httpClient.addNetworkInterceptor(interceptor).build();
+        }else if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP){
+            try {
+                client = httpClient.sslSocketFactory(getSSLConfig(context).getSocketFactory()).build();
+            } catch (CertificateException e) {
+            } catch (IOException e) {
+            } catch (KeyStoreException e) {
+            } catch (NoSuchAlgorithmException e) {
+            } catch (KeyManagementException e) {
+            } catch (java.security.cert.CertificateException e) {
+                e.printStackTrace();
+            }
+        }
 
-        OkHttpClient client = httpClient.addNetworkInterceptor(interceptor).build();
+
+
+
+
+
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl("https://dev-api.dismart.id")
@@ -124,5 +151,37 @@ public class ApiClient {
     public static Retrofit clearRetrofitInstance() {
         retrofit = null;
         return retrofit;
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private static SSLContext getSSLConfig(Context context) throws CertificateException, IOException,
+            KeyStoreException, NoSuchAlgorithmException, KeyManagementException, java.security.cert.CertificateException {
+
+        // Loading CAs from an InputStream
+        CertificateFactory cf = null;
+        cf = CertificateFactory.getInstance("X.509");
+
+        Certificate ca;
+        // I'm using Java7. If you used Java6 close it manually with finally.
+        try (InputStream cert = context.getResources().openRawResource(R.raw.dismart_id)) {
+            ca = cf.generateCertificate(cert);
+        }
+
+        // Creating a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        // Creating a TrustManager that trusts the CAs in our KeyStore.
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        // Creating an SSLSocketFactory that uses our TrustManager
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+
+        return sslContext;
     }
 }
