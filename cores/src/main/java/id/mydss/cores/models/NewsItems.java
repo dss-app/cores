3c2f0eb93fa3557532845987829b8b9df0ed7649
tri3ans4h <tri3ans4h@gmail.com>
2020-05-31 21:22:28 +0700

    newsitems model

--- cores/src/main/java/id/mydss/cores/models/NewsItems.java
+++ cores/src/main/java/id/mydss/cores/models/NewsItems.java
@@ -2,12 +2,10 @@ package id.mydss.cores.models;
 
 
 public class NewsItems {
-
     public String urlImg;
     public String txtTitle;
     public String txtDescription;
-
-    NewsItems(String urlImg, String txtTitle, String txtDescription) {
+    public NewsItems(String urlImg, String txtTitle, String txtDescription) {
         this.urlImg = urlImg;
         this.txtTitle = txtTitle;
         this.txtDescription = txtDescription;
